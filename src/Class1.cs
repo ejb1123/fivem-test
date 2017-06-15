d0490e467695a35f49d4413ab7bee3d10f6874e8
E.J. Bevenour <ejbevenour@live.com>
2017-06-15 08:38:33 -0400

    more changes

--- src/Class1.cs
+++ src/Class1.cs
@@ -27,38 +27,59 @@ namespace test
 
         private async Task OnTick()
         {
-            if (Game.IsControlJustPressed(0, Control.Cover))
+            if (Game.IsControlJustPressed(0, Control.SelectWeaponUnarmed))
             {
-                if (LocalPlayer.Character.IsInVehicle())
-                {
-                    RequestWarp(Game.PlayerPed.CurrentVehicle);
-                    var netidd = Function.Call<int>(Hash.NETWORK_GET_NETWORK_ID_FROM_ENTITY, LocalPlayer.Character.CurrentVehicle.Handle);
-                    Screen.ShowNotification($"Tell the other user to use the \"Z\" key and to enter \"{netidd}\"");
-                    return;
-                }
-                var k = new Model(VehicleHash.Police2);
-                await k.Request(-1);
-                //var veh = await World.CreateVehicle(VehicleHash.Police2, LocalPlayer.Character.Position);
-                var veh = new Vehicle(Function.Call<int>(Hash.CREATE_VEHICLE,
-                    VehicleHash.Police2,
-                    LocalPlayer.Character.Position.X,
-                    LocalPlayer.Character.Position.Y,
-                    LocalPlayer.Character.Position.Z,
-                    LocalPlayer.Character.Heading,
-                    false,
-                    true));
-                //veh.MarkAsNoLongerNeeded();
-                //k.MarkAsNoLongerNeeded();
-
-                LocalPlayer.Character.SetIntoVehicle(veh, VehicleSeat.Driver);
-                veh.RegisterAsNetworked();
-                var netid = veh.GetNetworkId();
-                veh.SetExistOnAllMachines(true);
-                RequestWarp(veh);
-                Screen.ShowNotification($"Tell the other user to use the \"Z\" key and to enter \"{netid}\"");
+                Game.DisableControlThisFrame(0,Control.SelectWeaponUnarmed);
+                SpawnVehicle(false,false);
+            }
+            if (Game.IsControlJustPressed(0, Control.SelectWeaponMelee))
+            {
+                Game.DisableControlThisFrame(0, Control.SelectWeaponMelee);
+                SpawnVehicle(false, true);
+            }
+            if (Game.IsControlJustPressed(0, Control.SelectWeaponShotgun))
+            {
+                Game.DisableControlThisFrame(0, Control.SelectWeaponShotgun);
+                SpawnVehicle(true, false);
+            }
+            if (Game.IsControlJustPressed(0, Control.SelectWeaponHeavy))
+            {
+                Game.DisableControlThisFrame(0, Control.SelectWeaponHeavy);
+                SpawnVehicle(true, true);
             }
         }
 
+        private async void SpawnVehicle(bool one,bool two)
+        {
+            Screen.ShowNotification($"Running {one},{two} test.");
+            if (LocalPlayer.Character.IsInVehicle())
+            {
+                RequestWarp(Game.PlayerPed.CurrentVehicle);
+                var netidd = Function.Call<int>(Hash.NETWORK_GET_NETWORK_ID_FROM_ENTITY, LocalPlayer.Character.CurrentVehicle.Handle);
+                Screen.ShowNotification($"Tell the other user to use the \"Z\" key and to enter \"{netidd}\"");
+                return;
+            }
+            var k = new Model(VehicleHash.Police2);
+            await k.Request(-1);
+            //var veh = await World.CreateVehicle(VehicleHash.Police2, LocalPlayer.Character.Position);
+            var veh = new Vehicle(Function.Call<int>(Hash.CREATE_VEHICLE,
+                VehicleHash.Police2,
+                LocalPlayer.Character.Position.X,
+                LocalPlayer.Character.Position.Y,
+                LocalPlayer.Character.Position.Z,
+                LocalPlayer.Character.Heading,
+                false,
+                false));
+            //veh.MarkAsNoLongerNeeded();
+            //k.MarkAsNoLongerNeeded();
+
+            LocalPlayer.Character.SetIntoVehicle(veh, VehicleSeat.Driver);
+            veh.RegisterAsNetworked();
+            var netid = veh.GetNetworkId();
+            veh.SetExistOnAllMachines(true);
+            RequestWarp(veh);
+            Screen.ShowNotification($"Tell the other user to use the \"Z\" key and to enter \"{netid}\"");
+        }
         private bool Doteleoprt(string str)
         {
             if (int.TryParse(str, out int num))
